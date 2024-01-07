24c7082fce42ab03884da06a040c0db520cbc27d
oz <oz.rnds@gmail.com>
2024-01-07 10:29:52 +0000

    refactor(db/media_service): Work with project-shkedia-models library. Add retry mechanism for HTTP calls

--- src/db/media_service.py
+++ src/db/media_service.py
@@ -3,11 +3,13 @@ logger = logging.getLogger(__name__)
 from typing import List
 from fastapi import Request
 import requests
+from requests.adapters import HTTPAdapter, Retry
 import json
 from pydantic import BaseModel
 
 from authentication.models import Token
-from models.media import MediaRequest, MediaDB, SearchResult
+from project_shkedia_models.media import MediaRequest, MediaDB, MediaIDs
+from project_shkedia_models.search import SearchResult
 
 class MediaDBService:
 
@@ -22,14 +24,14 @@ class MediaDBService:
     def is_ready(self):
         raise NotImplementedError("Is it necessary?")
 
-    def insert_media(self, token: Token, media: MediaRequest) -> MediaDB:
-        content = media.model_dump_json()
+    def insert_media(self, token: Token, media: MediaRequest) -> MediaIDs:
+        content = [media.model_dump()]
 
         insert_url = self.service_url+"/v1/media"
-        insert_response = requests.put(insert_url,json=json.loads(content), headers=token.get_token_as_header())
+        insert_response = requests.put(insert_url,json=content, headers=token.get_token_as_header())
 
         if insert_response.status_code == 200:
-            return MediaDB(**insert_response.json())
+            return [MediaIDs(**media_item) for media_item in insert_response.json()][0]
         raise Exception(insert_response.json()["detail"])
     
     def get(self, token: Token, media_id) -> MediaDB:
@@ -42,10 +44,23 @@ class MediaDBService:
 
     def search_media(self, token: Token, **kargs) -> SearchResult:
         insert_url = self.service_url+"/v1/media/search"
-        search_response = requests.get(insert_url,params=kargs, headers=token.get_token_as_header())
+
+        s = requests.Session()
+
+        retries = Retry(total=5,
+                backoff_factor=1,
+                status_forcelist=[429, 500, 502, 503, 504])
+        
+        s.mount('http://', HTTPAdapter(max_retries=retries))
+
+        search_response = s.get(insert_url,params=kargs, headers=token.get_token_as_header())
+
+        s.close()
 
         if search_response.status_code == 200:
             return SearchResult(**search_response.json())
+        if search_response.status_code == 401:
+            raise PermissionError(search_response.json()["detail"])
         if search_response.status_code == 404:
             return SearchResult(total_results_number=0, results=[])
         raise Exception(search_response.json()["detail"])
@@ -64,7 +79,7 @@ class MediaDBService:
         insert_response = requests.post(insert_url, json=json.loads(content), headers=token.get_token_as_header())
 
         if insert_response.status_code == 200:
-            return MediaDB(**insert_response.json())
+            return MediaIDs(**insert_response.json())
         raise Exception(insert_response.json()["detail"])
 
 
